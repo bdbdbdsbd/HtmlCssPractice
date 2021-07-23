ff0561e456380ffa857ef528c8b3b93ed9b52653
“bdbdbdsbd“ <1378984560@qq.com>
2021-07-23 21:52:29 +0800

    3

--- 3/JS/main.js
+++ 3/JS/main.js
@@ -1,6 +1,6 @@
 window.onload=function(){
-    Img=document.getElementById("img");
-window.addEventListener('scroll',function(){
+     let Img=document.getElementById("img");
+    window.addEventListener('scroll',function(){
     var value=window.scrollY;
     Img.style.height=(1000-value)/1000+'px';
 });
