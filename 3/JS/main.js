19c9a6c958c382c1a7defb5493014b00465a3a88
“bdbdbdsbd“ <1378984560@qq.com>
2021-07-23 21:51:21 +0800

    p

---
+++ 3/JS/main.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    Img=document.getElementById("img");
+window.addEventListener('scroll',function(){
+    var value=window.scrollY;
+    Img.style.height=(1000-value)/1000+'px';
+});
+}
