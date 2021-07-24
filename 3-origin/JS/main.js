6aeb47252cb15d836f383224b67ab0bed5b29476
“bdbdbdsbd“ <1378984560@qq.com>
2021-07-24 10:46:54 +0800

    3

--- 3-origin/JS/main.js
+++ 3-origin/JS/main.js
@@ -1,7 +1,16 @@
-var scroll=document.querySelector('.curve');
-window.onload=function(){
+var scroll1=document.getElementsByClassName('curve')[0];
+function roll(){
+    // var scroll1=document.querySelector('.curve');
     window.addEventListener('scroll',function(){
-        var value =1+window.scrollY/500;
-        scroll.style.transfrom =`scaleY(${value})`;
-    })
+        var value =1-window.scrollY/500;
+        scroll1.style.transfrom =`scaleY(${value})`;console.log(scroll1.style.transfrom);
+        // transfrom属性允许您对元素进行旋转、缩放、移动或倾斜。
+        // scaleY通过设置 Y 轴的值来定义缩放转换。
+        // $保留数字，再变成字符
+        //这里用的也不是单引号，而是``
+}
+);
+}
+window.onload=function(){
+    roll();
 }
--- 3/JS/main.js
+++ 3/JS/main.js
@@ -1,12 +1,13 @@
-var bgg=document.getElementsByClassName("imgg")[0];
+
 function rolli(){
     window.addEventListener('scroll',function(){
     var value=window.scrollY;
     bgg.style.height=(700-value)/700*212+'px';
     // 这里还可以使用
-    bgg.style.height='calc(40%-'(700-value)/700*212+'px'+')';
+    // bgg.style.height='calc(40%-'(700-value)/700*212+'px'+')';
     });
 }
 window.onload=function(){
+    var bgg=document.getElementsByClassName("imgg")[0];
     rolli();
 }
