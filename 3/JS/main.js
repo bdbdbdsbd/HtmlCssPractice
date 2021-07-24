aaf4e300813168af61605e4b44fad1762c498fa8
“bdbdbdsbd“ <1378984560@qq.com>
2021-07-24 09:49:12 +0800

    3-0

--- 3/JS/main.js
+++ 3/JS/main.js
@@ -3,6 +3,8 @@ function rolli(){
     window.addEventListener('scroll',function(){
     var value=window.scrollY;
     bgg.style.height=(700-value)/700*212+'px';
+    // 这里还可以使用
+    bgg.style.height='calc(40%-'(700-value)/700*212+'px'+')';
     });
 }
 window.onload=function(){
